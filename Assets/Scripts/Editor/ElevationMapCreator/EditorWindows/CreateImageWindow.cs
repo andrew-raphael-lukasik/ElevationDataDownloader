f45992e8f98b4cc6b60fb590df0257d9107b9921
Andrzej <andrew.r.lukasik@gmail.com>
2019-02-12 18:09:14 +0100

    required number of data points made more clear to understand

--- Assets/Scripts/Editor/ElevationMapCreator/EditorWindows/CreateImageWindow.cs
+++ Assets/Scripts/Editor/ElevationMapCreator/EditorWindows/CreateImageWindow.cs
@@ -14,7 +14,7 @@ namespace ElevationMapCreator
         MainWindow _owner = null;
 
         [System.NonSerialized] string _filePath = null;
-        int _fileNumValues;
+        int _numDataPoints;
         ElevationRange _elevationRange = new ElevationRange();
 
         #endregion
@@ -70,14 +70,15 @@ namespace ElevationMapCreator
                 EditorGUILayout.BeginHorizontal();
                 {
                     GUILayout.Label( "Data Points:" , GUILayout.Width(120f) );
-                    GUILayout.Label( _fileNumValues.ToString() , GUILayout.Width(100f) );
+                    GUILayout.Label( _numDataPoints.ToString() , GUILayout.Width(100f) );
                 }
                 EditorGUILayout.EndHorizontal();
 
 
                 GUILayout.FlexibleSpace();
 
-
+                
+                int requiredDataPoints = _owner.createImageSettings.resolution.latitude * _owner.createImageSettings.resolution.longitude;
                 EditorGUILayout.BeginHorizontal();
                 {
                     //
@@ -89,7 +90,7 @@ namespace ElevationMapCreator
                     GUILayout.Space( 20 );
                     
                     //
-                    GUILayout.Label( "Resolution:" , GUILayout.Width(30f) );
+                    GUILayout.Label( "Resolution:" , GUILayout.Width(70f) );
                     _owner.createImageSettings.resolution.latitude = Mathf.Clamp(
                         EditorGUILayout.IntField( _owner.createImageSettings.resolution.latitude , GUILayout.Width(60f) ) ,
                         1 ,
@@ -100,13 +101,19 @@ namespace ElevationMapCreator
                         1 ,
                         int.MaxValue
                     );
+                    GUILayout.Label( $" = { requiredDataPoints } data points required" );
+                    GUILayout.FlexibleSpace();
                 }
                 EditorGUILayout.EndHorizontal();
 
                 if( _filePath!=null )
                 {
-                    bool isSizeValid = (_owner.createImageSettings.resolution.latitude * _owner.createImageSettings.resolution.longitude) == _fileNumValues;
-                    if( isSizeValid==false ){ EditorGUILayout.HelpBox( "RESOLUTION DOES NOT MATCH SELECTED CSV FILE\ntwo resolution values when multiplied have to equal number of data points in the csv file (data point for every pixel)" , MessageType.Warning ); }
+                    int difference = _numDataPoints - requiredDataPoints;
+                    if( difference!=0 )
+                    {
+                        EditorGUILayout.HelpBox( "RESOLUTION DOES NOT MATCH SELECTED CSV FILE\ntwo resolution values when multiplied must be equal to number of data points in this csv file (one data point for every pixel).\nHence resolution numbers are wrong and/or file is incomplete" , MessageType.Warning );
+                        EditorGUILayout.HelpBox( $"TIP: CSV { (difference<0 ? "lacks" : "exceeds by") } { Mathf.Abs(difference) } entries" , MessageType.Info );
+                    }
                 }
 
                 GUI.enabled = _filePath!=null;
@@ -132,7 +139,7 @@ namespace ElevationMapCreator
         {
             //reset:
             _elevationRange.Reset();
-            _fileNumValues = 0;
+            _numDataPoints = 0;
 
             //read range:
             if(
@@ -159,7 +166,7 @@ namespace ElevationMapCreator
                     {
                         float elevation = float.Parse( line );
                         _elevationRange.Append( elevation );
-                        _fileNumValues++;
+                        _numDataPoints++;
                     }
                 }
                 catch ( System.Exception ex ) { Debug.LogException(ex); }
